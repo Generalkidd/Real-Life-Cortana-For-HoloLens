618086f14be7160dd2fcc0e72cf93aea966d3410
Generalkidd <Plasmadragons@hotmail.com>
2016-08-08 16:06:58 -0400

    Updated to latest version of Unity, fixed some bugs

---
+++ Assets/Scripts/Cortana.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cortana : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+}
