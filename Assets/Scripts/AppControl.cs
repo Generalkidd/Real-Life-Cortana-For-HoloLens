59d9d4d63fbffab86bb33cefff1ed2ebcb34a20b
Generalkidd <generalheed@rocketmail.com>
2016-08-15 19:16:03 -0400

    Updated to newest version of Unity, small improvements, initial demo release

--- Assets/Scripts/AppControl.cs
+++ Assets/Scripts/AppControl.cs
@@ -18,6 +18,7 @@ public class AppControl : MonoBehaviour
 
     void OnSwitch()
     {
-        
+        Destroy(GameObject.Find("Cortana 2"));
+        GameObject c = (GameObject)Instantiate(GameObject.Find("Cortana 1"), new Vector3(0, -0.5f, 1.549f), new Quaternion(0, 180, 0, 0));
     }
 }
